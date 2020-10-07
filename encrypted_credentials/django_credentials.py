8a250481fe0e0660e8248bc0f7d97253173e91be
Ian Jones <ian@cobi.co.uk>
2020-10-07 11:46:06 +0100

    1st Commit

---
+++ encrypted_credentials/django_credentials.py
@@ -0,0 +1,21 @@
+import os
+import json
+from .encrypted_file import get_decrypted_file
+from django.conf import settings
+
+
+class MissingCredentials(Exception):
+    pass
+
+
+def add_encrypted_settings(module_globals=None, settings_file=None, key=None):
+    if not settings_file:
+        settings_file = os.path.join(os.path.dirname(module_globals['__file__']), 'settings.json')
+    module_globals.update(json.loads(get_decrypted_file(settings_file, key)))
+
+
+def get_credentials(credential_name):
+    credential_file = settings.CREDENTIAL_FILES.get(credential_name)
+    if not credential_name:
+        raise MissingCredentials(f"Cannot find '{credential_name}' in CREDENTIAL_FILES")
+    return get_decrypted_file(os.path.join(settings.CREDENTIAL_FOLDER, credential_file))
---
+++ encrypted_credentials/encrypted_file.py
@@ -0,0 +1,48 @@
+import os
+from cryptography.fernet import Fernet
+
+
+class NoEncryptedData(Exception):
+    pass
+
+
+def get_decrypted_file(original_filename, settings_key=None, encrypted_filename=None):
+    if not settings_key:
+        settings_key = get_key()
+        if not settings_key:
+            return
+    encryption = Fernet(settings_key)
+    if not encrypted_filename:
+        encrypted_filename = f'{original_filename}.enc'
+    encrypted_data = None
+    if os.path.isfile(encrypted_filename):
+        with open(encrypted_filename, 'rb') as f:
+            encrypted_data = encryption.decrypt(f.read())
+
+    initial_data = None
+    if os.path.isfile(original_filename):
+        with open(original_filename, 'rb') as f:
+            initial_data = f.read()
+
+    if not initial_data:
+        if encrypted_data is None:
+            raise NoEncryptedData(f'Cannot find data for {original_filename}')
+        return encrypted_data
+    elif initial_data != encrypted_data:
+        encrypted_data = encryption.encrypt(initial_data)
+        with open(encrypted_filename, 'wb') as f:
+            f.write(encrypted_data)
+    return initial_data
+
+
+def get_key():
+    key = os.environ.get('SETTINGS_KEY', None)
+    if not key:
+        print(f'No SETTINGS_KEY set in Environment for secure settings\n'
+              f'  A new project can use: SETTINGS_KEY={random_key()}')
+        return
+    return key
+
+
+def random_key():
+    return Fernet.generate_key().decode("utf-8")
